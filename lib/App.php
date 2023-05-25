c29dd04bf7c25f8e7f6d72b6eb247aa99d4b1264
Sergei Nazarenko <nazares@icloud.com>
2023-05-26 00:57:19 +0300

    add command registration method

--- lib/App.php
+++ lib/App.php
@@ -4,16 +4,28 @@ namespace Clipper;
 
 class App
 {
-    protected OutputHelper $output;
+    public OutputHelper $output;
+    protected $registry = [];
 
     public function __construct()
     {
         $this->output = new OutputHelper();
     }
 
+    public function registerCommand($command, $callback)
+    {
+        $this->registry[$command] = $callback;
+    }
+
     public function runCommand(array $argv)
     {
-        $name = $argv[1] ?? 'World';
-        $this->output->print(sprintf("Hello, %s!", $name));
+        $commandName = $argv[1] ?? 'help';
+        $command = $this->registry[$commandName] ?? null;
+
+        if ($command === null) {
+            $this->output->print(sprintf("ERROR: Command \"%s\" not found.", $commandName));
+            exit;
+        }
+        call_user_func($command, $argv);
     }
 }
