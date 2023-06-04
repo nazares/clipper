aca3c9e8b2c1faf598791e5c9533d54ad5c29937
Sergei Nazarenko <nazares@icloud.com>
2023-06-04 21:52:41 +0300

    Refactor App.php to use controller

--- lib/App.php
+++ lib/App.php
@@ -2,60 +2,146 @@
 
 namespace Clipper;
 
+use Clipper\Command\CommandCall;
+use Clipper\Command\CommandRegistry;
+use Clipper\Output\CliPrinter;
+
 class App
 {
-    /** @var OutputHelper $output */
-    protected OutputHelper $output;
+    /** @var  string  */
+    protected $app_signature;
 
-    /** @var CommandRegistry $commandRegistry */
-    protected CommandRegistry $commandRegistry;
+    /** @var  array */
+    protected $services = [];
 
-    /**
-     * App constructor
-     *
-     * @return void
-     * */
-    public function __construct()
+    /** @var array  */
+    protected $loaded_services = [];
+
+    public function __construct(array $config = null)
     {
-        $this->output = new OutputHelper();
-        $this->commandRegistry = new CommandRegistry();
+        $config = array_merge([
+            'app_path' => __DIR__ . '/../app/Command',
+            'theme'    => 'regular',
+        ], $config);
+
+        $this->setSignature('./minicli help');
+
+        $this->addService('config', new Config($config));
+        $this->addService('command_registry', new CommandRegistry($this->config->app_path));
+        $this->addService('printer', new CliPrinter());
     }
 
     /**
-     * CLI commad registration
-     *
-     * @param string $command
-     * @param callable $callback
+     * Magic method implements lazy loading for services.
+     * @param string $name
+     * @return ServiceInterface|null
+     */
+    public function __get($name)
+    {
+        if (!array_key_exists($name, $this->services)) {
+            return null;
+        }
+
+        if (!array_key_exists($name, $this->loaded_services)) {
+            $this->loadService($name);
+        }
+
+        return $this->services[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param ServiceInterface $service
+     */
+    public function addService($name, ServiceInterface $service)
+    {
+        $this->services[$name] = $service;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function loadService($name)
+    {
+        $this->loaded_services[$name] = $this->services[$name]->load($this);
+    }
+
+    /**
+     * @return OutputInterface
+     */
+    public function getPrinter()
+    {
+        return $this->printer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSignature()
+    {
+        return $this->app_signature;
+    }
+
+    /**
      * @return void
      */
-    public function registerCommand(string $command, callable $callback): void
+    public function printSignature()
     {
-        $this->commandRegistry->registerCommand($command, $callback);
+        $this->getPrinter()->display(sprintf("usage: %s", $this->getSignature()));
+    }
+    /**
+     * @param string $app_signature
+     */
+    public function setSignature($app_signature)
+    {
+        $this->app_signature = $app_signature;
+    }
+
+    /**
+     * @param string $name
+     * @param callable $callable
+     */
+    public function registerCommand($name, $callable)
+    {
+        $this->command_registry->registerCommand($name, $callable);
     }
 
     /**
      * @param array $argv
-     * @return void
      */
-    public function runCommand(array $argv): void
+    public function runCommand(array $argv = [])
     {
-        $commandName = $argv[1] ?? 'help';
-        $command = $this->commandRegistry->getCommand($commandName);
+        $input = new CommandCall($argv);
 
-        if ($command === null) {
-            $this->output->print(sprintf("ERROR: Command \"%s\" not found.", $commandName));
+        if (count($input->args) < 2) {
+            $this->printSignature();
             exit;
         }
-        call_user_func($command, $argv);
+
+        $controller = $this->command_registry->getCallableController($input->command, $input->subcommand);
+
+        if ($controller instanceof ControllerInterface) {
+            $controller->boot($this);
+            $controller->run($input);
+            $controller->teardown();
+            exit;
+        }
+
+        $this->runSingle($input);
     }
 
     /**
-     *
-     * @param string $out
-     * @return void
+     * @param CommandCall $input
      */
-    public function print(string $out)
+    protected function runSingle(CommandCall $input)
     {
-        return $this->output->print($out);
+        try {
+            $callable = $this->command_registry->getCallable($input->command);
+            call_user_func($callable, $input);
+        } catch (\Exception $e) {
+            $this->getPrinter()->display("ERROR: " . $e->getMessage());
+            $this->printSignature();
+            exit;
+        }
     }
 }
