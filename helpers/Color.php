7c7872a494774c92d6890a73b268858d4e60a98a
Sergei Nazarenko <nazares@icloud.com>
2023-06-04 22:01:45 +0300

    add color enum

---
+++ helpers/Color.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace helpers;
+
+enum Color: int
+{
+    case Black = 0;
+    case Red = 1;
+    case Green = 2;
+    case Yellow = 3;
+    case Blue = 4;
+    case Magenta = 5;
+    case Cyan = 6;
+    case White = 7;
+    case Default = 9;
+}
